922b1966bf7c1b8d3be96c54561927970d072123
Julio Roman <julioromanfernandez@gmail.com>
2023-03-28 18:25:21 +0200

    Creacion automatica de hileras en gazebo FUNCIONA

--- agribot_croprow_generator/GazeboCropRowGenerator.py
+++ agribot_croprow_generator/GazeboCropRowGenerator.py
@@ -10,9 +10,9 @@ def y(x, m, b):
 
 Row_Num = 3
 hilera = 2 #Numero de plantas en paralelo por cada hilera
-Max_BPR = 50 # maximum Big plants in each row
+Max_BPR = 5 # maximum Big plants in each row
 Max_SPR = 0 # maximum Small plant in each row
-Row_lenght = 50 # in meters
+Row_lenght = 5 # in meters
 Random_noise_magnitude = 1 # maximum random noise magitude in meters
 
 SP_H = 0.110674
@@ -50,14 +50,13 @@ for j in range(Row_Num):
         Y_P[i+ii] = [y(x, CropRow_Slope, i * CropRow_Offset + y_offset)  for x in X_P[i]] #JRF #+ abs(random.gauss(Pose_Y,Sigma_Plant_dis))
         Y_W[i] = [y(x, CropRow_Slope, i * CropRow_Offset + y_offset) + abs(random.gauss(Pose_Y,Sigma_Weed_dis)) for x in X_W[i]]
         plt.scatter(X_P[i+ii], Y_P[i+ii], c='g')
-        plt.scatter(X_W[i], Y_W[i], c='r')
+        # plt.scatter(X_W[i], Y_W[i], c='r')
         y_offset = i * CropRow_Offset + y_offset       #JRF   Guardo la posicion Y de la ultima fila de plantas puesta, para poder poner la soguiente fila justo a 1.5 metros
     
     ii = ii + 2
     y_offset = y_offset + 1.5       #JRF
    
 # print(X_P)
-#plt.scatter(X_P, Y_P, c='g')
 plt.show()
 
 if GENERATE_FILE:
@@ -85,20 +84,20 @@ if GENERATE_FILE:
 
     # Pose = "\t\t\t\t<pose frame=''>" + str(Pose_X) + " " + str(Pose_Y) + " " + str(Pose_H) + " 0 -0 0</pose>\n"
     model_def = "\n\t\t</model>\n"
-    scale = "\t\t\t<scale>1 1 1</scale>\n"
-    BP_link_name = "\t\t\t<link name='link_0'>\n"
+    scale = "\t\t\t\t<scale>1 1 1</scale>\n"
+    BP_link_name = "\t\t\t\t<link name='link_0'>\n"
     SP_link_name = "\t\t\t<link name='link_0'>\n"
 
     # Big plants
-    for i in range(Row_Num):
+    for i in range(2*Row_Num):
         for x in range(Max_BPR):
-            BP_name = "\n\t\t<model name='big_plant_" + str(x+ i*Max_BPR) + "'>\n"
+            BP_name = "\n\n\t\t\t<model name='big_plant_" + str(x+ i*Max_BPR) + "'>\n"
             new_Headers.write(BP_name)
-            Pose = "\t\t\t<pose frame=''>" + str(X_P[i][x]) + " " + str(Y_P[i][x]) + " " + str(0) + " 0 -0 0</pose>\n"
+            Pose = "\t\t\t\t<pose frame=''>" + str(X_P[i][x]) + " " + str(Y_P[i][x]) + " " + str(0) + " 0 -0 0</pose>\n"
             new_Headers.write(Pose)
             new_Headers.write(scale)
             new_Headers.write(BP_link_name)
-            Pose = "\t\t\t\t<pose frame=''>" + str(X_P[i][x]) + " " + str(Y_P[i][x]) + " " + str(BP_H) + " 0 -0 0</pose>\n"
+            Pose = "\t\t\t\t\t<pose frame=''>" + str(X_P[i][x]) + " " + str(Y_P[i][x]) + " " + str(BP_H) + " 0 -0 0</pose>\n"
             new_Headers.write(Pose)
             new_Headers.write(BP_HEADER_content)
 
@@ -108,6 +107,7 @@ if GENERATE_FILE:
             new_Models.write(Pose)
             new_Models.write(model_def)
 
+      
         #  Small plants
         for x in range(Max_SPR):
             SP_name = "\n<model name='small_plant_" + str(x+i*Max_SPR) + "'>\n"
@@ -134,12 +134,15 @@ if GENERATE_FILE:
     new_Models.close()
 
     # Output files
+    P0_path = "farm_P0.xml"
     P1_path = "farm_P1.xml"
     P2_path = "farm_P2.xml"
     P3_path = "farm_P3.xml"
+    farm_P0 = open(P0_path,'r')
     farm_P1 = open(P1_path,'r')
     farm_P2 = open(P2_path,'r')
     farm_P3 = open(P3_path,'r')
+    farm_P0_content = farm_P0.read()
     farm_P1_content = farm_P1.read()
     farm_P2_content = farm_P2.read()
     farm_P3_content = farm_P3.read()
@@ -154,6 +157,7 @@ if GENERATE_FILE:
     Final_path = 'FarmWithCropRow.world'
     Final_World = open(Final_path,'w')
 
+    Final_World.write(farm_P0_content)
     Final_World.write(farm_P1_content)
     Final_World.write(new_Headers_read)
     Final_World.write(farm_P2_content)
